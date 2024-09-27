1166dfc7699516f7d96697d1bd728210c30a75bb
leelu74 <leeladhar679@gmail.com>
2024-09-28 01:09:44 +0530

    Started the prakamya

---
+++ Prakamya.py
@@ -0,0 +1,4 @@
+#  Prakamya
+# Prakamya siddhi provides wide range of abilities to its user. It allows its user to increase their life length dramatically, 
+# teleportation, ability to live underwater. In the general sense prakamya enabels the user to adapt to the circumstances. 
+# Various rishimunis, chiranjivis and demigods had this ashta siddhi in the past as they could live under water and extend their lives to exceptionally longg time.`
